b631f8dfd8b99498b463803f40559db211b41c13
azoor-desu <31021840+azoor-desu@users.noreply.github.com>
2025-04-09 02:38:53 +0800

    WIP laying out stuff to work on

--- src/defines.h
+++ src/defines.h
@@ -5,46 +5,33 @@
 #include <string> // for std::string and std::wstring
 #include <string_view> // for std::string_view
 
-constexpr float PI = 3.141592653f;
-constexpr double PI_DBL = 3.14159265358979323846;
-constexpr float INF_FLOAT = std::numeric_limits<float>::infinity();
-constexpr double INF_DBL = std::numeric_limits<double>::infinity();
-constexpr float NATURAL_LOG_OF_2 = 0.693147181f;
-constexpr double NATURAL_LOG_OF_2_DBL = 0.693147181;
-constexpr float SQR_ROOT_2 = 1.41421356237f;
-constexpr double SQR_ROOT_2_DBL = 1.41421356237;
-
-// Converts Radian to Degree units. (double)
-static inline double RAD2DEG_DBL(double rad) { return rad * 180.0 / PI_DBL; }
-
-// Converts Radian to Degree units. (float)
-static inline float RAD2DEG(float rad) { return rad * 180.f / PI; }
-
-// Converts Degree to Radian units. (double)
-static inline double DEG2RAD_DBL(double deg) { return deg * PI_DBL / 180.0; }
-
-// Converts Degree to Radian units. (float)
-static inline float DEG2RAD(float deg) { return deg * PI / 180.f; }
-
 #ifndef TRUE
 #define TRUE 1
 #endif
-
 #ifndef FALSE
 #define FALSE 0
 #endif
-
 #ifndef UNREFERENCED_PARAMETER
 #define UNREFERENCED_PARAMETER(P) (P)
 #endif
-
 #ifndef UNUSED_PARAM
 #define UNUSED_PARAM(P) (P)
 #endif
-
 #ifndef UNUSED
 #define UNUSED(P) (P)
 #endif
+
+constexpr float embPI = 3.141592653f;
+constexpr double embPI_F64 = 3.14159265358979323846;
+constexpr float embLn2 = 0.693147181f;
+constexpr double embLn2_F64 = 0.693147181;
+constexpr float embSqrt2 = 1.41421356237f;
+constexpr double embSqrt2_F64 = 1.41421356237;
+
+constexpr double RAD2DEG(double rad) { return rad * 180.0 / embPI_F64; }
+constexpr float RAD2DEG(float rad) { return rad * 180.f / embPI; }
+constexpr double DEG2RAD(double deg) { return deg * embPI_F64 / 180.0; }
+constexpr float DEG2RAD(float deg) { return deg * embPI / 180.f; }
 
 // Primitive using renames. Extended from "vanilla" primitive types
 using embBitmask8 = uint8_t; // 8-bit bitmask for bit operations
@@ -66,6 +53,8 @@ using embU64 = uint64_t; // 64-bits, 8 bytes, unsigned
 
 using embF32 = float; // 32-bits, 4 bytes
 using embF64 = double; // 64-bits, 8 bytes
+constexpr float embF32_INF = std::numeric_limits<float>::infinity();
+constexpr double embF64_INF = std::numeric_limits<double>::infinity();
 
 // strings and stuff
 using embStr = std::string;
